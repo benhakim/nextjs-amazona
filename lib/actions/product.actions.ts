43c66569b9f2b3ffc67c542c14992b4b3003287f
benhakim <lhkim@web.de>
2025-09-26 11:11:09 +0200

    03

---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,33 @@
+"use server"
+import Product from "@/lib/db/models/product.model";
+import {connectToDatabase} from "@/lib/db";
+
+
+export async function getAllCategories() {
+    const categories = await Product.find({ isPublished: true }).distinct('category')
+    return categories
+}
+export async function getProductsForCard({
+    tag,
+    limit = 4
+}: {
+    tag: string,
+    limit?: number })
+{
+    await connectToDatabase()
+    const products = await Product.find({
+    tag: { $in: [tag] },
+    isPublished: true
+    },
+        {
+            name: 1,
+            href: { $concat: ['/product/', '$slug' ] },
+            image: { $arrayElemAt: ['$images', 0] },
+        }
+) .sort({ createdAt: 'desc'})
+    .limit(limit)
+    return JSON.parse(JSON.stringify(products)) as {
+    name: string
+    href: string
+    image: string}[]
+}
